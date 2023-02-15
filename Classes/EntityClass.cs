fce317bf9714ba4ecfdba2c98ced241c7b1d682b
123 <123123123>
2023-02-15 11:14:02 +0300

    Добавьте файлы проекта.

---
+++ Classes/EntityClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShurBolCofeeHouse.DataBase;
+
+namespace ShurBolCofeeHouse.Classes
+{
+    internal class EntityClass
+    {
+        public static Entities Context { get; } = new Entities();
+    }
+}
