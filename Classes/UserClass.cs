f87acc20942efdc7033ba51c40c3dfddd6573c62
123 <123123123>
2023-04-07 10:07:38 +0300

    Навигация

---
+++ Classes/UserClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShurBolCofeeHouse.DataBase;
+
+namespace ShurBolCofeeHouse.Classes
+{
+    internal class UserClass
+    {
+        public static Staff Staff { get; set; } = new Staff();
+        public static Client Client { get; set; } = new Client();
+    }
+}
--- Windows/AuthWindow.xaml.cs
+++ Windows/AuthWindow.xaml.cs
@@ -39,16 +39,36 @@ namespace ShurBolCofeeHouse
 
         private void btnAuth_Click(object sender, RoutedEventArgs e)
         {
-            var authUser = Context.Client.ToList().Where(i => i.Login == tboxAuthLogin.Text && i.Password == tboxAuthPas.Text).FirstOrDefault();
-            if (authUser != null)
-            {
-                MessageBox.Show("Успешно!");
-                // переход на окно пользователя
+            var authClient = Context.Client.ToList().Where(i => i.Login == tboxAuthLogin.Text && i.Password == tboxAuthPas.Text).FirstOrDefault();
+            var authStaff = Context.Staff.ToList().Where(i => i.Login == tboxAuthLogin.Text && i.Password == tboxAuthPas.Text).FirstOrDefault();
+            
+            if (authClient != null)
+            {   //преход на окна клиента
+                UserClass.Client = authClient;
+                ClientWindow clientWindow = new ClientWindow();
+                clientWindow.Show();
+                this.Close();
+                MessageBox.Show($"Успешно! Здравствуйте, клиент {authClient.FirstName} {authClient.LastName}!") ;
             }
-            else
-            {
-                MessageBox.Show("Неверный логин или пароль");
+            else if(authStaff != null)
+            { //преход на окна  сотрудников
+                UserClass.Staff = authStaff;
+                DirectorWindow directorWindow = new DirectorWindow();
+                directorWindow.Show();
+                this.Close();
+                MessageBox.Show($"Успешно! Здравствуйте, директор {authStaff.FirstName} {authStaff.LastName}!");
             }
+            else MessageBox.Show("Неверный логин или пароль");
+
+        }
+
+        private void tboxAuthLogin_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void tboxAuthPas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
 
         }
     }
